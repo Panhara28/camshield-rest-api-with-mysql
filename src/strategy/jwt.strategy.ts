69afcccd82858a734eda00e4f4abe3a60c005950
panhara28 <titpanhara@gmail.com>
2025-05-24 09:53:47 +0700

    Add Passport JWT and Strategy

---
+++ src/strategy/jwt.strategy.ts
@@ -0,0 +1,41 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UserPayloadDto } from 'src/users/dto';
+
+interface UserPayload {
+  sub: number;
+  email: string;
+}
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    config: ConfigService,
+    private prisma: PrismaService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('JWT_SECRET') as string,
+    });
+  }
+
+  async validate(payload: UserPayload) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: payload.sub,
+      },
+    });
+
+    if (!user) {
+      return null;
+    }
+
+    const { password, ...result } = user;
+
+    return result;
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { UserPayloadDto } from './dto';
 import { UsersService } from './users.service';
+import { JwtGuard } from 'src/guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @UseGuards(JwtGuard)
   @Post('create')
   createUser(@Body() payload: UserPayloadDto) {
     return this.usersService.createUser(payload);
